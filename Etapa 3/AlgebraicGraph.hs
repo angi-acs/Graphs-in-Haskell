ab9f917be7b13682a8d8be85f54b28bd2025a52b
Corina-Angi Balanescu <corina.balanescu@stud.acs.upb.ro>
2023-01-26 13:35:35 +0200

    Initial commit

---
+++ Etapa 3/AlgebraicGraph.hs
@@ -0,0 +1,256 @@
+module AlgebraicGraph where
+
+import qualified Data.Set as S
+
+data AlgebraicGraph a
+    = Empty
+    | Node a
+    | Overlay (AlgebraicGraph a) (AlgebraicGraph a)
+    | Connect (AlgebraicGraph a) (AlgebraicGraph a)
+
+-- (1, 2), (1, 3)
+angle :: AlgebraicGraph Int
+angle = Connect (Node 1) (Overlay (Node 2) (Node 3))
+
+-- (1, 2), (1, 3), (2, 3)
+triangle :: AlgebraicGraph Int
+triangle = Connect (Node 1) (Connect (Node 2) (Node 3))
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor grafului.
+
+    Hint: S.union
+-}
+nodes :: Ord a => AlgebraicGraph a -> S.Set a
+nodes Empty = S.empty
+nodes (Node x) = S.fromList [x]
+nodes (Overlay x y) = S.union (nodes x) (nodes y)
+nodes (Connect x y) = S.union (nodes x) (nodes y)
+
+{-
+    *** TODO ***
+
+    Mulțimea arcelor grafului.
+
+    Hint: S.union, S.cartesianProduct
+-}
+edges :: Ord a => AlgebraicGraph a -> S.Set (a, a)
+edges Empty = S.empty
+edges (Node _) = S.empty
+edges (Overlay x y) = S.union (edges x) (edges y)
+edges (Connect x y) = S.union (S.cartesianProduct (nodes x) (nodes y))
+                              (S.union (edges x) (edges y))
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor înspre care pleacă arce dinspre nodul curent.
+
+    ATENȚIE! NU folosiți funcția edges definită mai sus, pentru că ar genera
+    prea multe muchii inutile.
+-}
+outNeighbors :: Ord a => a -> AlgebraicGraph a -> S.Set a
+outNeighbors _ Empty = S.empty
+outNeighbors _ (Node _) = S.empty
+outNeighbors node (Overlay x y) = S.union (outNeighbors node x) (outNeighbors node y)
+outNeighbors node (Connect x y)
+    | S.member node (nodes x) = S.union (nodes y) (outNeighbors node y)
+    | otherwise = S.union (outNeighbors node x) (outNeighbors node y)
+
+{-
+    *** TODO ***
+
+    Mulțimea nodurilor dinspre care pleacă arce înspre nodul curent.
+
+    ATENȚIE! NU folosiți funcția edges definită mai sus, pentru că ar genera
+    prea multe muchii inutile.
+-}
+inNeighbors :: Ord a => a -> AlgebraicGraph a -> S.Set a
+inNeighbors _ Empty = S.empty
+inNeighbors _ (Node _) = S.empty
+inNeighbors node (Overlay x y) = S.union (inNeighbors node x) (inNeighbors node y)
+inNeighbors node (Connect x y)
+    | S.member node (nodes y) = S.union (nodes x) (inNeighbors node y)
+    | otherwise = S.union (inNeighbors node x) (inNeighbors node y)
+
+{-
+    *** TODO ***
+
+    Instanțiați clasa Num cu tipul (AlgebraicGraph a), astfel încât:
+    - un literal întreg să fie interpretat ca un singur nod cu eticheta egală
+      cu acel literal
+    - operația de adunare să fie intepretată ca Overlay
+    - operația de înmulțire să fie interpretată drept Connect.
+
+    Celelalte funcții din clasă nu sunt relevante. Veți obține warning-uri
+    pentru neimplementarea lor, dar puteți să le ignorați.
+
+    După instanțiere, veți putea evalua în consolă expresii ca:
+
+    > 1 :: AlgebraicGraph Int
+    Node 1
+    
+    > 1*(2+3) :: AlgebraicGraph Int
+    Connect (Node 1) (Overlay (Node 2) (Node 3))
+-}
+instance Num a => Num (AlgebraicGraph a) where
+    fromInteger = Node . fromInteger
+    (+) = Overlay
+    (*) = Connect
+
+{-
+    *** TODO ***
+
+    Instanțiați clasa Show cu tipul (AlgebraicGraph a), astfel încât
+    reprezentarea sub formă de șir de caractere a unui graf să reflecte
+    expresiile aritmetice definite mai sus. Puteți pune un nou rând de paranteze
+    la fiecare subexpresie compusă.
+
+    Exemple:
+
+    > Node 1
+    1
+
+    > Connect (Node 1) (Overlay (Node 2) (Node 3))
+    (1*(2+3))
+-}
+instance Show a => Show (AlgebraicGraph a) where
+    show Empty = ""
+    show (Node x) = show x
+    show (Overlay x y) = "(" ++ show x ++ "+" ++ show y ++ ")"
+    show (Connect x y) = "(" ++ show x ++ "*" ++ show y ++ ")"
+
+{-
+    *** TODO ***
+
+    Observați că instanța predefinită de Eq pentru tipul (AlgebraicGraph a)
+    nu surprinde corect egalitatea a două grafuri, deoarece același graf
+    conceptual poate avea două descrieri simbolice diferite.
+    
+    Prin urmare, instanțiați clasa Eq cu tipul (AlgebraicGraph a), astfel încât
+    să comparați propriu-zis mulțimile de noduri și de arce.
+
+    Exemple:
+
+    > Node 1 == 1
+    True
+
+    > Node 1 == 2
+    False
+
+    > angle == 1*2 + 1*3
+    True
+
+    > triangle == (1*2)*3
+    True
+-}
+instance Ord a => Eq (AlgebraicGraph a) where
+    g1 == g2 = nodes g1 == nodes g2 && edges g1 == edges g2
+
+{-
+    *** TODO ***
+
+    Extinde un graf existent, atașând noi subgrafuri arbitrare în locul nodurilor
+    individuale. Funcția primită ca prim parametru determină această
+    corespondență între noduri și subgrafuri. Observați că tipul etichetelor
+    noi (b) poate diferi de al etichetelor vechi (a).
+
+    Exemplu:
+
+    > extend (\n -> if n == 1 then 4+5 else Node n) $ 1*(2+3)
+    ((4+5)*(2+3))
+-}
+extend :: (a -> AlgebraicGraph b) -> AlgebraicGraph a -> AlgebraicGraph b
+extend f = helper
+    where
+        helper Empty = Empty
+        helper (Node x) = f x
+        helper (Overlay x y) = Overlay (helper x) (helper y)
+        helper (Connect x y) = Connect (helper x) (helper y)
+
+{-
+    *** TODO ***
+
+    Divizează un nod în mai multe noduri, cu eliminarea nodului inițial.
+    Arcele în care era implicat vechiul nod trebuie să devină valabile
+    pentru noile noduri.
+    
+    Implementați splitNode folosind extend!
+-}
+splitNode :: Eq a
+          => a                 -- nodul divizat
+          -> [a]               -- nodurile cu care este înlocuit
+          -> AlgebraicGraph a  -- graful existent
+          -> AlgebraicGraph a  -- graful obținut
+splitNode node targets = extend f
+    where
+        f = \n -> if n == node then g else Node n
+        g = foldl (\acc x -> Overlay (Node x) acc) Empty targets
+
+{-
+    *** TODO ***
+
+    Instanțiați clasa Functor cu constructorul de tip AlgebraicGraph, astfel
+    încât să puteți aplica o funcție pe toate etichetele unui graf.
+    fmap reprezintă generalizarea lui map pentru orice fel de structură.
+
+    Implementați fmap folosind extend!
+
+    Exemplu:
+
+    > fmap (+ 10) $ 1*(2+3) :: AlgebraicGraph Int
+    (11*(12+13))
+-}
+instance Functor AlgebraicGraph where
+    -- fmap :: (a -> b) -> AlgebraicGraph a -> AlgebraicGraph b
+    fmap f = extend (Node . f)
+
+{-
+    *** TODO ***
+
+    Îmbină mai multe noduri într-unul singur, pe baza unei proprietăți
+    respectate de nodurile îmbinate, cu eliminarea acestora. Arcele în care
+    erau implicate vechile noduri vor referi nodul nou.
+
+    Implementați mergeNodes folosind fmap!
+-}
+mergeNodes :: (a -> Bool)       -- proprietatea îndeplinită de nodurile îmbinate
+           -> a                 -- noul nod
+           -> AlgebraicGraph a  -- graful existent
+           -> AlgebraicGraph a  -- graful obținut
+mergeNodes prop node = fmap f
+    where
+        f = \n -> if prop n then node else n
+
+{-
+    *** TODO ***
+
+    Filtrează un graf, păstrând doar nodurile care satisfac proprietatea dată.
+
+    Implementați filterGraph folosind extend!
+    
+    Exemplu:
+
+    > nodes $ filterGraph odd $ 1*(2+3)
+    fromList [1,3]
+
+    > edges $ filterGraph odd $ 1*(2+3)
+    fromList [(1,3)]
+-}
+filterGraph :: (a -> Bool) -> AlgebraicGraph a -> AlgebraicGraph a
+filterGraph prop = extend f
+    where
+        f = \n -> if prop n then Node n else Empty
+
+{-
+    *** TODO ***
+
+    Întoarce graful rezultat prin eliminarea unui nod și a arcelor în care
+    acesta este implicat. Dacă nodul nu există, se întoarce același graf.
+
+    Implementați removeNode folosind filterGraph!
+-}
+removeNode :: Eq a => a -> AlgebraicGraph a -> AlgebraicGraph a
+removeNode node = filterGraph (/= node)
